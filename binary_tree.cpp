66fad42b911bb58174bf3fa3dac67fbca191edd5
mo-jo-dev <b210055@nitsikkim.ac.in>
2024-10-13 11:46:01 +0530

    bt code

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -6,15 +6,15 @@ struct Node{
     struct Node *right;
     Node (int val){
         data = val;
-        left = null;
-        right = null;
+        left = nullptr;
+        right = nullptr;
     }
 };
 
 int main(){
-    struct node *root = new Node(1);
+    Node *root = new Node(1);
     root->left = new Node(2);
-    root->right = new Nose(3);
+    root->right = new Node(3);
     root->left->left = new Node(4);
     root->left->right = new Node(5);
 }
